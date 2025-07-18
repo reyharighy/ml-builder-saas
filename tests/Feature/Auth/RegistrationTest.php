5c91d804d23325dd1aff0e2ca3a341ddb9f85dcc
reyharighy <arighymoch@gmail.com>
2025-07-19 03:46:24 +0700

    Fix the registration feature test

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -10,8 +10,8 @@ test('new users can register', function () {
     $response = $this->post('/register', [
         'name' => 'Test User',
         'email' => 'test@example.com',
-        'password' => 'password',
-        'password_confirmation' => 'password',
+        'password' => '@Password1',
+        'password_confirmation' => '@Password1',
     ]);
 
     $this->assertAuthenticated();
