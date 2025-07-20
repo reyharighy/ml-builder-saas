a2fd62c446e4ea10f7861262f4b610be52734a09
reyharighy <arighymoch@gmail.com>
2025-07-21 01:26:19 +0700

    Provide feature tests related to routes inside AuthenticatedSessionController

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -3,15 +3,18 @@
 use App\Models\User;
 
 test('login screen can be rendered', function () {
-    $response = $this->get('/login');
+    $response = $this->get(route('login'));
 
     $response->assertStatus(200);
 });
 
 test('users can authenticate using the login screen', function () {
-    $user = User::factory()->create();
+    $user = User::factory()->create([
+        'email' => 'test@example.com',
+        'password' => '@Password123',
+    ]);
 
-    $response = $this->post('/login', [
+    $response = $this->post(route('login'), [
         'email' => $user->email,
         'password' => '@Password123',
     ]);
@@ -20,11 +23,40 @@ test('users can authenticate using the login screen', function () {
     $response->assertRedirect(route('dashboard', absolute: false));
 });
 
-test('users can not authenticate with invalid password', function () {
-    $user = User::factory()->create();
+test('users can not authenticate with invalid email', function () {
+    User::factory()->create([
+        'email' => 'correct@email.com',
+    ]);
 
-    $this->post('/login', [
+    $this->post(route('login'), [
+        'email' => 'invalid@email.com',
+        'password' => '@Password123',
+    ]);
+
+    $this->assertGuest();
+});
+
+test('users can not authenticate with invalid password', function () {
+    $user = User::factory()->create([
+        'password' => '@Password123',
+    ]);
+
+    $this->post(route('login'), [
         'email' => $user->email,
+        'password' => 'wrong-password',
+    ]);
+
+    $this->assertGuest();
+});
+
+test('users can not authenticate with invalid email and password', function () {
+    $user = User::factory()->create([
+        'email' => 'correct@email.com',
+        'password' => '@Password123',
+    ]);
+
+    $this->post(route('login'), [
+        'email' => 'invalid@email.com',
         'password' => 'wrong-password',
     ]);
 
@@ -34,7 +66,7 @@ test('users can not authenticate with invalid password', function () {
 test('users can logout', function () {
     $user = User::factory()->create();
 
-    $response = $this->actingAs($user)->post('/logout');
+    $response = $this->actingAs($user)->post(route('logout'));
 
     $this->assertGuest();
     $response->assertRedirect('/');
