49ef3d8784e75380308ebf987358254b2e91b5d8
reyharighy <arighymoch@gmail.com>
2025-07-21 01:11:05 +0700

    Provide feature tests to route dashboard

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -3,14 +3,16 @@
 use App\Models\User;
 
 test('guests are redirected to the login page', function () {
-    $response = $this->get('/dashboard');
-    $response->assertRedirect('/login');
+    $response = $this->get(route('dashboard'));
+
+    $this->assertGuest();
+    $response->assertRedirect(route('login'));
 });
 
 test('authenticated users can visit the dashboard', function () {
     $user = User::factory()->create();
     $this->actingAs($user);
 
-    $response = $this->get('/dashboard');
+    $response = $this->get(route('dashboard'));
     $response->assertStatus(200);
 });
