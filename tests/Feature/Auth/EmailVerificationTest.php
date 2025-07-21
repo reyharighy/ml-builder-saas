2f8c0abee29c6f01b76f501849bba6fa21a56957
reyharighy <arighymoch@gmail.com>
2025-07-21 23:07:08 +0700

    Add more to email verification feature test

--- tests/Feature/Auth/EmailVerificationTest.php
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -2,15 +2,85 @@
 
 use App\Models\User;
 use Illuminate\Auth\Events\Verified;
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\URL;
 
-test('email verification screen can be rendered', function () {
-    $user = User::factory()->unverified()->create();
+test('user is redirected to email verification screen if their email is unverified upon visiting dataset path', function () {
+    $user = User::factory()->unverified()->create([
+        'password' => '@Password123',
+    ]);
 
-    $response = $this->actingAs($user)->get(route('verification.notice'));
+    $response = $this->actingAs($user)->post(route('login'), [
+        'email' => $user->email,
+        'password' => '@Password123',
+    ]);
 
-    $response->assertStatus(200);
+    $this->assertAuthenticated();
+
+    $response = $this->actingAs($user)
+        ->get(route('datasets.index'))
+        ->assertRedirect(route('verification.notice'));
+
+    $response->assertStatus(302);
+});
+
+test('user is redirected to email verification screen if their email is unverified upon visiting project path', function () {
+    $user = User::factory()->unverified()->create([
+        'password' => '@Password123',
+    ]);
+
+    $this->actingAs($user)->post(route('login'), [
+        'email' => $user->email,
+        'password' => '@Password123',
+    ]);
+
+    $this->assertAuthenticated();
+
+    $response = $this->actingAs($user)
+        ->get(route('projects.index'))
+        ->assertRedirect(route('verification.notice'));
+
+    $response->assertStatus(302);
+});
+
+test('email verification notification is sent to unverified user', function () {
+    Notification::fake();
+
+    $user = User::factory()->unverified()->create([
+        'password' => '@Password123',
+    ]);
+
+    $this->actingAs($user)->post(route('login'), [
+        'email' => $user->email,
+        'password' => '@Password123',
+    ]);
+
+    $this->assertAuthenticated();
+
+    $this->actingAs($user)
+        ->post(route('verification.send'))
+        ->assertSessionHas('status', 'verification-link-sent');
+
+    Notification::assertSentTo($user, VerifyEmail::class);
+});
+
+test('email verification notification is not sent to verified user', function () {
+    $user = User::factory()->create([
+        'password' => '@Password123',
+    ]);
+
+    $this->actingAs($user)->post(route('login'), [
+        'email' => $user->email,
+        'password' => '@Password123',
+    ]);
+
+    $this->assertAuthenticated();
+
+    $this->actingAs($user)
+        ->post(route('verification.send'))
+        ->assertRedirect(route('dashboard', absolute: false));
 });
 
 test('email can be verified', function () {
